b79d35e2eb8c44af0a6bed626af90fef89afcf23
Kimberly Gadeberg <kimberlygadeberg10@gmail.com>
2026-04-25 16:28:32 -0500

    Add app setup and service ticket model

--- mechanic_shop_app/models.py
+++ mechanic_shop_app/models.py
@@ -21,3 +21,16 @@ class Vehicle(db.Model):
     year = db.Column(db.Integer, nullable=False)
     license_plate = db.Column(db.String(20), nullable=False)
     color = db.Column(db.String(30))
+    
+class ServiceTicket(db.Model):
+    __tablename__ ="service_tickets"
+    
+    ticket_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.customer_id"), nullable=False)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicles.vehicle_id"), nullable=False)
+    date_opened = db.Column(db.Date, nullable=False)
+    date_closed = db.Column(db.Date)
+    service_description = db.Column(db.String(255), nullable=False)
+    problem_reported = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    total_cost = db.Column(db.Numeric(10, 2), nullable=False)
