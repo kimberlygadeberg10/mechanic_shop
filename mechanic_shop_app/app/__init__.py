15b3dd1afa018d41eef7478db5c4b12e3b2cab2e
Kimberly Gadeberg <kimberlygadeberg10@gmail.com>
2026-04-26 18:49:09 -0500

    Update database configuration after password change

--- mechanic_shop_app/app/__init__.py
+++ mechanic_shop_app/app/__init__.py
@@ -4,7 +4,7 @@ from app.routes import register_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:Phoenix0350*@127.0.0.1:3306/mechanic_shop_db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:Phoenix0350#@127.0.0.1:3306/mechanic_shop_db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
